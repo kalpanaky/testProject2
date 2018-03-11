e997aaa140acf26eef0492e5c94d998ea0fce948
Kalpana Yadav <kalpana.yadav@gmail.com>
2018-03-11 01:17:00 -0600

    change3

--- testProject2/src/testPackage/testClass.java
+++ testProject2/src/testPackage/testClass.java
@@ -2,4 +2,13 @@ package testPackage;
 
 public class testClass {
 int num1 = 3;
+String str = "Kalpana";
+int num2 = num1;
+public void myMethod()
+{
+ 	
+ System.out.println("value"+num2);
+}
+
+
 }
